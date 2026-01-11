4c8e8de9ebda1a45ff7454323525b7027033644c
brandon10118 <202310118@fit.edu.ph>
2026-01-11 12:07:20 +0800

    Add debugbar JSON files for request profiling
    - Created debugbar_1767947528.716345.json for profiling the request at timestamp 1767947528.
    - Created debugbar_1767948068.771259.json for profiling the request at timestamp 1767948068.
    - Both files include detailed metrics such as timers, database queries, logs, views, files, routes, and events.

--- app/Views/include/head_view.php
+++ app/Views/include/head_view.php
@@ -6,8 +6,29 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    
+    <!-- Puter.js SDK -->
+    <script src="https://js.puter.com/v2/"></script>
+
+    <style>
+        html, body {
+            height: 100%;
+        }
+        body {
+            display: flex;
+            flex-direction: column;
+        }
+        .container-fluid {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+        }
+        footer {
+            margin-top: auto;
+        }
+    </style>
 
     <title><?= esc($title); ?></title>
 </head>
 <body>
-    <div class="container">
+    <div class="container-fluid">
